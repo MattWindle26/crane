5e4bba5482354fc7e61f91cf412ef9ede775026b
Matt Windle <mattwindle@Matts-MacBook-Pro.local>
2017-09-01 09:12:47 +0100

    First Commit

---
+++ dist/includes/header.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Your title here</title>
+    <link rel="stylesheet" href="/css/main.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  </head>
+  <body>
+
+  <nav class="site-nav">
+    <div class="site-container nav-container">
+      <div class="logo"><a href="#">logo</a></div>
+
+      <div class="mobile-nav">
+        <div class="inner">
+          <span class="icon-bar top"></span>
+          <span class="icon-bar middle"></span>
+          <span class="icon-bar bottom"></span>
+        </div>
+      </div>
+
+      <div class="site-nav__items" style="display:none">
+        <a href="#">Lorem</a>
+        <a href="#">Lorem</a>
+        <a href="#">Lorem</a>
+        <a href="#">Lorem</a>
+        <a href="#">Lorem</a>
+        <a href="#">Lorem</a>
+      </div>
+    </div>
+  </nav>
